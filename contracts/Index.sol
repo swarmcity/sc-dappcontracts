09d40f33e0bed6c4b0a92e18a531e467018d2e4c
Luke Kayaba <brainjackersvr@gmail.com>
2017-07-05 20:45:19 -0500

    Update Index.sol

--- contracts/Index.sol
+++ contracts/Index.sol
@@ -2,12 +2,12 @@ pragma solidity ^0.4.8;
 
 contract Index {	
   address[] public hashtags; //List of hashtags' addresses.
-	string[] public names; //List of hashtags' names.
-	address[] public factories; //List of hashtags' factories.
+  string[] public names; //List of hashtags' names.
+  address[] public factories; //List of hashtags' factories.
 
-	function addMe(string name, address factory) {
-   	hashtags.push(msg.sender);
-   	names.push(name);
+  function addMe(string name, address factory) {
+    hashtags.push(msg.sender);
+    names.push(name);
     factories.push(factory);
   }
 }
