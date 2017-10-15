fffc77157199d07ca998e15092d7b6d685c6b757
asdfadsf <yas@example.com>
2017-10-15 18:26:57 +0100

    moved rep tokens to seperate deploy

--- contracts/Hashtag.sol
+++ contracts/Hashtag.sol
@@ -31,38 +31,24 @@ contract Hashtag is Ownable {
 	MiniMeToken SeekerRep;
 	string public metadataHash;
 
-	/// Events
-	event DealRegistered(address dealContract);
+        event DealRegistered(address dealContract);
 	event ProviderRepAdded(address to, uint amount);
 	event SeekerRepAdded(address to, uint amount);
 
-	function Hashtag(address _token, address _tokenfactory, string _name, uint _commission, address _dealFactory, string _metadataHash){
+	function Hashtag(address _token, address _tokenfactory, 
+			string _name, uint _commission, 
+			address _dealFactory, string _metadataHash, 
+			address _ProviderRep, address _SeekerRep){
 		/// @notice The function that creates the hashtag
 		name = _name;
 		/// @notice The dealFactory that can mint rep on this hashtag and sets the deal type
 		dealFactory = _dealFactory;
 		/// @notice The provider reputation token is created
-		ProviderRep = new MiniMeToken(
-			_tokenfactory,
-			0,
-            0,
-            _name,
-            0,
-            'SWR',
-            false
-		);
+		ProviderRep = MiniMeToken(_ProviderRep);
 
 		/// @notice The seeker reputation token is created
-		SeekerRep = new MiniMeToken(
-			_tokenfactory,
-			0,
-            0,
-            _name,
-            0,
-            'SWR',
-            false
-		);
 
+		SeekerRep = MiniMeToken(_SeekerRep);
 		token = MiniMeToken(_token);
 		metadataHash = _metadataHash;
 		commission = _commission;
