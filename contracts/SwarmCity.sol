8907be42c8e1e7234d20c86c28f5bcb7794baaba
kingflurkel <mikethuy@gmail.com>
2017-10-30 11:56:17 -0500

    SwarmCity hashtag index contract

---
+++ contracts/SwarmCity.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.15;
+
+import './Ownable.sol';
+
+contract SwarmCity is Ownable {
+
+	string public SwarmCityHash;
+	mapping public (address => bool) vouchers;
+	uint public numVouchers;
+
+	function setSwarmCity(string _hash) onlyOwner external {
+		numVouchers = 0;
+		delete vouchers;
+		SwarmCityHash = _hash;
+		vouchSwarmCity(msg.sender);
+	}
+
+	function vouchSwarmCity(string _ipfshash) external {
+		vouchers[msg.sender] = true;
+		numVouchers++;
+	}
+
+	function checkSwarmCity(address _address) returns bool external {
+		return vouchers[_address];
+	}
+
+}
